13047dd83d57dda19179eea665ba7853ddb1b084
Harttu Toivonen <harttu.toivonen@raseko.fi>
2018-11-21 08:34:03 +0200

    UusiAlku

--- Program.cs
+++ Program.cs
@@ -1,97 +1,61 @@
 ﻿using System;
-using System.Collections.Generic;
 
-namespace UusiTekstinLukuJaKirjoitus
+namespace tekstinLukuJakirjoitus
 {
     class Henkilo
     {
-        // olion ominaisuudet (properties)
         public string nimi;
-        public string ikä;
-        public string harrastus;
-        // lisää tähän string-muuttujat, johon laitetaan harrastukset
-        public void esitteleItsesti() {
-            Console.WriteLine("Nimi:" + nimi + " ikä:" + ikä + " harrastukset:" + harrastus);
-        }
-        // olion funktiot (eli metodit)
-        public Henkilo(string animi, string aikä, string aharrastus) // Muodostin (constructor) funktio
+        public string ika;
+        public string harrastukset;
+        public void tulostaTyyppi()
         {
-            nimi = animi;
-            ikä = aikä;
-            harrastus = aharrastus;
+            Console.WriteLine("Nimi:" + nimi + " ikä:" + ika + " harrastukset:" + harrastukset);
+        }
+        public Henkilo(string n = "", string i = "", string h = "")
+        {
+            nimi = n;
+            ika = i;
+            harrastukset = h;
         }
     }
-
     class Program
     {
         static void Main(string[] args)
         {
-            /*
-            Console.WriteLine("Valitse toiminto:");
-            Console.WriteLine("1.Tulosta tiedot");
-            Console.WriteLine("2.Lisää tieto");
-            Console.WriteLine("3.Lue tiedostosta");
-            Console.WriteLine("4.Kirjoita tiedostoon");
-            Console.WriteLine("5.Lopeta");
-            int valinta = int.Parse(Console.ReadLine());
-            switch (valinta)
+            while (true)
             {
-                case 1:
-                    Console.WriteLine("Valitsit toiminnon 1");
-                    break;
-            }
-            */
-            // Tämän koodin pitäis toimia kun luokka on valmis
-            // Lisää mattille yksi harrastus tässä
-            Henkilo matti = new Henkilo("Matti", "13", "Ponit");
-            Henkilo marleena = new Henkilo("Marleena", "30", "Ompelu");
-            Henkilo sirpa = new Henkilo("Sirpa", "54", "Toimintaelokuvat");
-            /*
-            Console.WriteLine(matti.nimi + " " + matti.ikä + " " + matti.harrastus);
+                Console.WriteLine("Valitse toiminto:");
+                Console.WriteLine("1.Tulosta tiedot");
+                Console.WriteLine("2.Lisää tieto");
+                Console.WriteLine("3.Lue tiedostosta");
+                Console.WriteLine("4.Kirjoita tiedostoon");
+                Console.WriteLine("5.Lopeta");
 
-            Console.WriteLine(marleena.nimi + " " + marleena.ikä + " " + marleena.harrastus);
+                int valinta = int.Parse(Console.ReadLine());
 
-            // Googlaa c# class methods examples
-            marleena.esitteleItsesti();
-            sirpa.esitteleItsesti();
-            */
-            // TEHTÄVÄ
-            // Etsi miten C#:lla luetaan tietoa tiedostosta
-            // Lue data.txt ohjelman muistiin ja tulosta se konsoliin
-            // Google-apu: C# read textfile 
-            string[] lines = System.IO.File.ReadAllLines(@"C:\Users\raikas\Desktop\data.txt");
-            Console.WriteLine("Tiedostossa on rivejä: " + lines.Length);
-            // Pistä toimimaan siten, että rivi määrä voi muuttua
-            for ( int i = 0; i < lines.Length; i++)// <- googlaa: how to find length of an array c#
-            {
-                Console.WriteLine( lines[i] );
-            }
-            //TEHTÄVÄ 2:
-            // Etsi miten c#:lla kirjoitetaan tiedostoon seuraavat
-            // tehtävä 1:
-            // tiedostoon yksiRiviEsimerkki.txt
-            string tämärivitiedostoon = "Pekka tykkää hillomunkeista";
-            // tehtävä 2:
-            // montaRiviäEsimerkki.txt
-            string[] nämärivittiedostoon = { "Pekka tykkää hillomunkeista", "Kalle baanaaneista" };
+                switch (valinta)
+                {
+                    case 1:
+                        Console.WriteLine("1.Valittu");
+                        break;
+                    case 2:
+                        Console.WriteLine("2.Valittu");
+                        break;
+                    case 3:
+                        Console.WriteLine("3.Valittu");
+                        break;
+                    case 4:
+                        Console.WriteLine("4.Valittu");
 
-            /*
-            //                         0                           1
-            string[] lines2 = { "Moro Joulupukki,", "Tässä on pieni kirje sinulle",
-                //   2          3
-                "Terveisin", "Matti" };
-            for (int i = 0; i < 4; i++)
-            {
-                Console.WriteLine(lines2[i]);
-            }
-            /*
-            Console.WriteLine(lines[0]);
-            Console.WriteLine(lines[1]);
-            Console.WriteLine(lines[2]);
-            Console.WriteLine(lines[3]);
-            */
-
-            // Tehtävä tulsota lines muuttuja sekä for:illa että foreach:lla
-        }
+                        break;
+                    case 5:
+                        System.Environment.Exit(1);
+                        break;
+                    default:
+                        Console.WriteLine("Tarkista valinta");
+                        break;
+                } // switch
+            } // while            
+        } // main
     }
 }
